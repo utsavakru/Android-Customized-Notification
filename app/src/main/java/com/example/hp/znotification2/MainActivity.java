a60de25020db65b5f2aac7605bb7b2da137b6cb4
Utsav Akru <49266434+utsavakru@users.noreply.github.com>
2019-06-25 11:59:24 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/hp/znotification2/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.hp.znotification2;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public void createNotification(View view) {
+        // Prepare intent which is triggered if the
+        // notification is selected
+        Intent intent = new Intent(this, NotificationReceiverActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+        // Build notification
+        // Actions are just fake
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle("New mail from " + "test@gmail.com")
+                .setContentText("Subject").setSmallIcon(R.drawable.logo)
+                .setContentIntent(pIntent)
+                .addAction(R.drawable.logo, "Call", pIntent)
+                .addAction(R.drawable.logo, "More", pIntent)
+                .addAction(R.drawable.logo, "And more", pIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // hide the notification after its selected
+        noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(0, noti);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/hp/znotification2/NotificationReceiverActivity.java
@@ -0,0 +1,15 @@
+package com.example.hp.znotification2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationReceiverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+    }
+}
+
+
+
